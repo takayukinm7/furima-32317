2ff94336c0c6ac4a06663e46422d1809415b71ca
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-02-03 22:51:40 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :familyname, presence: true
+  validates :firstname, presence: true
+  validates :familynamekana, presence: true
+  validates :firstnamekana, presence: true
+  validates :date, presence: true
+
+  has_many :items
+  has_many :comments
+  has_many :buyers
+end
