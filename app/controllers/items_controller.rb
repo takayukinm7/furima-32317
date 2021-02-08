11935a8978d2f76155131f22682583f44a43f8bc
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-02-09 08:00:06 +0900

    商品登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :image, :price, :text, :user, :category_id, :state_id, :cost_id, :prefecture_id, :day_id)
   end
 end
