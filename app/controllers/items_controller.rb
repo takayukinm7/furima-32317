62e2ef45f9c36e2fc3caa8a47c588f026becfa9b
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-02-12 16:38:32 +0900

    商品一覧表示機能の修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @items = Item.all
     @items = Item.order('created_at DESC')
   end
 
@@ -10,6 +9,8 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
+    @item.user = current_user
     if @item.save
       redirect_to root_path
     else
