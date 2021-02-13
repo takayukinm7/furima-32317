a85ddb68f4630ce1bff8e3994d56d8a35d1fea62
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-02-13 19:09:19 +0900

    商品編集機能修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only:[:edit]
+  before_action :move_to_index, only:[:edit, :update]
+  before_action :get_id, only:[:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -48,5 +46,9 @@ class ItemsController < ApplicationController
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
+  end
+
+  def get_id
+    @item = Item.find(params[:id])
   end
 end
