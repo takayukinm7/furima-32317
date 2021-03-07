088116ff7c6845978dc4a0c253b953e39d04dbd1
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-03-07 18:20:59 +0900

    商品購入機能の修正９

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :get_id, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
