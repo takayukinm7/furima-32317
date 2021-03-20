6c7f13bc5c9b1ba269cfb52a725b83bde107387d
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-03-20 16:47:53 +0900

    空のコミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :get_id, only: [:show, :edit, :update, :destroy ]
+  before_action :get_id, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
